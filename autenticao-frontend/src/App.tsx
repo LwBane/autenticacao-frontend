8f56761ee55869a1985e51fc1bf1923b604e4ad7
LwBane <emiliepessoa8@gmail.com>
2026-04-08 21:18:46 -0300

    feat: token de acesso e refresh no localstorage

--- autenticao-frontend/src/App.tsx
+++ autenticao-frontend/src/App.tsx
@@ -1,72 +1,36 @@
-import axios from 'axios'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+import { Login } from './Login'
+import { Registrar } from './Registrar';
+import { Home } from './Home';
 
 function App() {
+  const [dataLogin, setDataLogin] = useState<any>(null);
+  const [registrar, setRegistrar] = useState(false);
 
-  // === Cadastro === 
-
-  const [cadastroEmail, setcadastroEmail] = useState("")
-  const [cadastroSenha, setcadastroSenha] = useState("")
-  const [dataRegister, setDataRegister] = useState(null)
-
-  const cadastrar = async () => {
-    try {
-      const response = await axios.post("http://localhost:3000/cadastro", {
-        email,
-        senha
+  useEffect(() => {
+    const accessToken = localStorage.getItem("tokenAcesso")
+    const refreshToken = localStorage.getItem("tokenRefresh")
+    const hasTokens = accessToken && refreshToken
+    if (hasTokens) {
+      setDataLogin({
+        accessToken,
+        refreshToken
       })
-      if (response?.data) {
-        setDataLogin(response.data)
-      }
-    } catch (error) {
-      console.log(error)
-      alert("Erro ao fazer o login, verifique suas credenciais!")
     }
+  }, [])
 
-  }
-
-  // === Login  === 
-
-  const [email, setEmail] = useState("")
-  const [senha, setSenha] = useState("")
-  const [dataLogin, setDataLogin] = useState(null)
-
-  const logar = async () => {
-    try {
-      const response = await axios.post("http://localhost:3000/login", {
-        email,
-        senha
-      })
-      if (response?.data) {
-        setDataLogin(response.data)
-      }
-    } catch (error) {
-      console.log(error)
-      alert("Erro ao fazer o login, verifique suas credenciais!")
-    }
-  }
   return (
     <>
-      {dataLogin !== null ? (
-        <div className='flex flex-col bg-gray-400 w-full pt-32 items-center h-screen'>
-          {/*Boas vindas*/}
-          <h1 className='text-2xl font-semibold text-white'>Seja bem-vindo fulaninho</h1>
-          <button className='bg-red-800 px-5 py-2 text-white rounded-2xl' onClick={() => setDataLogin(null)}>Sair</button>
-        </div>
-      ) : (
-        < div className='flex flex-col bg-gray-400 w-full justify-center items-center h-screen'>
-          {/* Formulário login */}
-          <form className='flex  gap-2 w-full flex-col items-center justify-center'>
-            <label htmlFor="email">Email</label>
-            <input type="email" onChange={(e) => setEmail(e.target.value)} name="email" id="" />
-            <label htmlFor="password">Senha</label>
-            <input type="password" onChange={(e) => setSenha(e.target.value)} name="password" id="" />
-          </form>
-          <button onClick={logar} className='bg-gray-800 px-5 py-2 text-white rounded-2xl'>Login</button>
-          <button onClick={cadastrar} className='bg-gray-800 px-5 py-2 text-white rounded-2xl'>Não tenho conta</button>
-        </div >
-      )
+      {dataLogin !== null ? <Home setDataLogin={setDataLogin} /> : registrar ?
+        <Registrar setRegistrar={setRegistrar} />
+        :
+        <Login
+          setDataLogin={setDataLogin}
+          setRegistrar={setRegistrar}
+        />
+
       }
+
     </>
   )
 }
